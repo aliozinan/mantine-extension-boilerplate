9ec7a1afc8f6e0b8f94cab00b7bf84af79f6b98c
Ali Ozinan <aozinan@gmail.com>
2024-04-29 15:01:14 +0100

    darkmode & LTR for storybook

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,16 @@ import dts from 'vite-plugin-dts';
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig, UserConfig } from 'vite';
+import stylelint from 'vite-plugin-stylelint';
 
 export default defineConfig({
   base: './',
-  plugins: [dts({ rollupTypes: true }), react()],
+  plugins: [dts({ rollupTypes: true }), stylelint({ fix: true, cache: true }), react()],
   build: {
     sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: '@kodkisot/mantine-v7-extension-boilerplate',
+      name: 'mantine-extension-boilerplate',
       formats: ['es', 'cjs', 'umd', 'iife'],
       fileName: (format) => `index.${format}.js`,
     },
