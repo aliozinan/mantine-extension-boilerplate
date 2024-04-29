80f9cd99e1b0ce87dcf28b2b79652d0ca062bac9
Ali Ozinan <aozinan@gmail.com>
2024-04-29 16:58:16 +0100

    TS error fix

--- src/Component.types.ts
+++ src/Component.types.ts
@@ -3,13 +3,13 @@ import { DefaultProps, MantineNumberSize, Selectors } from '@mantine/core';
 import { ClassNames, Styles } from '@mantine/styles';
 import useStyles from './Component.styles';
 
-export type ComponentStylesNames = Selectors<typeof useStyles>;
-
 export interface ComponentContext {
   classNames?: ClassNames<ComponentStylesNames>;
   styles?: Styles<ComponentStylesNames>;
   unstyled?: boolean;
 }
+
+export type ComponentStylesNames = Selectors<typeof useStyles>;
 
 export interface ComponentProps extends DefaultProps<ComponentStylesNames> {
   /** Padding from theme.spacing, or any valid CSS value to set padding */
--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import { Component as _Component } from './Component';
 
 export const Component = _Component;
 
-export type { ComponentStylesNames, ComponentProps } from './Component';
+export type { ComponentStylesNames, ComponentProps } from './Component.types';
 export type { ComponentStylesParams } from './Component.styles';
